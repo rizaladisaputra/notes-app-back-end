59003dfcbaf0ad2c77f56c0a15dad09034639c35
rizal <rizaladi@maxxitani.id>
2025-09-03 00:02:16 +0700

    create structure folder

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+import daStyle from "eslint-config-dicodingacademy";
+
+export default [
+  defineConfig([
+    {
+      files: ["**/*.{js,mjs,cjs}"],
+      plugins: { js },
+      extends: ["js/recommended"],
+      languageOptions: { globals: globals.node },
+    },
+    { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  ]),
+  daStyle,
+];
